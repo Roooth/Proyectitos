0831267fa2b8c20863281d9f8e4532a0404d307c
Roooth <142053739+Roooth@users.noreply.github.com>
2023-09-08 14:15:42 -0500

    Generador
    Ya Sirve :D

--- Clases xd/Clasesiniciales/GeneradorNombres/GeneradorCiudad.cs
+++ Clases xd/Clasesiniciales/GeneradorNombres/GeneradorCiudad.cs
@@ -8,6 +8,20 @@ namespace GeneradorNombres
 {
     class GeneradorCiudad
     {
-        public string City;
+        public string[] InicialNombres = { "Bro", "Pol", "For", "Anc", "Enh", "Gri", "Dar","Col", "Mol", "Rot", "Gre", "Blu","Cre",
+                                           "Gol", "Nol", "Sou", "Eas", "Wes", "Sil", "Lit", "Clo", "Cal", "Sle", "Spl", "Gla", "Sal"};
+        public string[] InicialApellidos = { "ham", "ton", "dunn", "gham", "don", "chester", "field", "pool", "york", "dam", "yo", "hal", "ray"
+                                            ,"rachi", "bul", "hagen", "bourne", "lin", "ris", "end", "to", "rid", "enna", "co", "bing", "burgh"};
+        public string[] MesNacimiento = { "Enchanted Fire", "Green Sea", "Deep Mountain", "Devious Souls","Glowing Heat", "Fallen Elders"
+                , "Lost Souls", "Royal Witches", "Shallow Fox", "Rotten Fruit", "Golden Rock", "Mystic Warriors" };
+
+        public void generar(int nombre,int apellido,int mes) 
+        {
+            if (nombre > 26) { Console.WriteLine("ERROR"); }
+            if (apellido > 26) { Console.WriteLine("ERROR"); }
+            if (mes > 12) { Console.WriteLine("ERROR"); }
+            Console.WriteLine("Welcome to " + InicialNombres[nombre - 1] + InicialApellidos[apellido -1] + ", The City Of The " + MesNacimiento[mes - 1]);
+        }
+
     }
 }
--- Clases xd/Clasesiniciales/GeneradorNombres/Program.cs
+++ Clases xd/Clasesiniciales/GeneradorNombres/Program.cs
@@ -10,14 +10,23 @@ namespace GeneradorNombres
     {
         static void Main(string[] args)
         {
+            //Areglos
             string[] InicialNombres = { "Bro", "Pol", "For", "Anc", "Enh", "Gri", "Dar" };
             string[] InicialApellidos = { "ham", "ton", "dunn", "gham", "don", "chester", "field" };
             string[] MesNacimiento = { "Enchanted Fire", "Green Sea", "Deep Mountain", "Devious Souls",
             "Glowing Heat", "Fallen Elders", "Lost Souls" };
 
 
+            //Aqui estan 3 distintas "ciudades" generadas a partir de los arreglos
+            Console.WriteLine("Welcome to " + InicialNombres[0] + InicialApellidos[2] + ", The City Of The " + MesNacimiento[5]);
+            Console.WriteLine("Welcome to " + InicialNombres[6] + InicialApellidos[1] + ", The City Of The " + MesNacimiento[3]);
+            Console.WriteLine("Welcome to " + InicialNombres[2] + InicialApellidos[4] + ", The City Of The " + MesNacimiento[1]);
+
+            //La instancia generadora con el metodo, la de a de veras
             
-            Console.WriteLine("Welcome to " + InicialNombres[0] + InicialApellidos[2] + " , The City Of The " + MesNacimiento[5]);
+            GeneradorCiudad ciudad = new GeneradorCiudad();
+            ciudad.generar(1,2,3);
+
             Console.ReadLine();
         }
     }
