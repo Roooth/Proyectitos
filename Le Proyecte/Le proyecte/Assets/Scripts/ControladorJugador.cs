16947743d6cb149c7f7705b50f86ec5a50f80d6a
Roooth <142053739+Roooth@users.noreply.github.com>
2023-09-08 15:48:13 -0500

    El cavernicola se mueve :skull:
    Ahora puede caminar pero no saltar

---
+++ Le Proyecte/Le proyecte/Assets/Scripts/ControladorJugador.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJugador : MonoBehaviour
+{
+    private Rigidbody2D miCuerpo;
+    public float velocidadCaminar = 3;
+    private Animator miAnimador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Al atribuirle MiCuerpo le asigno el componente rigid body 2d a este personaje
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velActualVert = miCuerpo.velocity.y;
+        //Leo si el usuario esta presionando un eje horizontal en las flechas
+        float movHoriz = Input.GetAxis("Horizontal");
+        if (movHoriz > 0)// A la derecha
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+                                            //El 3 es hardcore, asi es parametrizado :D
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velActualVert, 0);
+            miAnimador.SetBool("Caminando", true);
+        }
+        else if (movHoriz < 0)//A la izquierda
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velActualVert, 0);
+            miAnimador.SetBool("Caminando", true);
+        }
+        else//Quieto
+        {
+            miCuerpo.velocity = new Vector3(0, velActualVert, 0);
+            miAnimador.SetBool("Caminando", false);
+        }
+    }
+}
