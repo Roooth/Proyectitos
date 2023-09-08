06b95ba72b0151072fb6a41fb54f0a70aa653119
Roooth <142053739+Roooth@users.noreply.github.com>
2023-09-08 10:21:44 -0500

    Areglos
    No he podido agregar la clase ni método generar...debo solicitar aclaraciones porque no lo he entendido de todo
    Aun asi el codigo funciona pero no es  como debe ser...creo

--- Clases xd/Clasesiniciales/GeneradorNombres/Program.cs
+++ Clases xd/Clasesiniciales/GeneradorNombres/Program.cs
@@ -10,8 +10,15 @@ namespace GeneradorNombres
     {
         static void Main(string[] args)
         {
+            string[] InicialNombres = { "Bro", "Pol", "For", "Anc", "Enh", "Gri", "Dar" };
+            string[] InicialApellidos = { "ham", "ton", "dunn", "gham", "don", "chester", "field" };
+            string[] MesNacimiento = { "Enchanted Fire", "Green Sea", "Deep Mountain", "Devious Souls",
+            "Glowing Heat", "Fallen Elders", "Lost Souls" };
 
 
+            
+            Console.WriteLine("Welcome to " + InicialNombres[0] + InicialApellidos[2] + " , The City Of The " + MesNacimiento[5]);
+            Console.ReadLine();
         }
     }
 }
