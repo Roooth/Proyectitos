5945439937854c33e1449cdc8eae7bf2c96bc318
Roooth <142053739+Roooth@users.noreply.github.com>
2023-10-19 15:48:16 -0500

    SE AREGLO SOLO :skull: :skull: :skull:
    Ahora recibe daño pero es un cavernicola inmortal xd, tengo tarea de hacer una death zone con agua

--- Le Proyecte/Le proyecte/Assets/Scripts/Personaje.cs
+++ Le Proyecte/Le proyecte/Assets/Scripts/Personaje.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Personaje : MonoBehaviour
+    
 {
     // Start is called before the first frame update
     public int hp = 90;
     public int hpMax = 100;
     public int score = 0;
     public int vidas = 3;
+    private Animator miAnimadori;
     //agregar etique para vidas, ya tenemos la cosa aqui ahora en el contolador de ui hacer lo mismo que el score
     void Start()
     {
-        
+        miAnimadori = GetComponent<Animator>();
     }
+
+    public void hacerDanio(int puntos, GameObject atacante)
+    {
+        print(name + "recibe daño de " + puntos + "por " + atacante.name);
+        hp = hp - puntos;
+        miAnimadori.SetTrigger("Ouch");
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
