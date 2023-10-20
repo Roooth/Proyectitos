553a95a82aaa37f8a4d1b8a731ec57f5e976f681
Roooth <142053739+Roooth@users.noreply.github.com>
2023-10-20 10:55:37 -0500

    tarea
    a

--- Le Proyecte/Le proyecte/Assets/Scripts/ZonaMuerte.cs
+++ Le Proyecte/Le proyecte/Assets/Scripts/ZonaMuerte.cs
@@ -6,6 +6,7 @@ public class ZonaMuerte : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collisionMuerte)
     {
-        
+        print(name + " hizo colision con " + collisionMuerte.gameObject.name + "se ha caido del mapa");
+        //hacer que se muera
     }
 }
